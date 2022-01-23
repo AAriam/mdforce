247e610d7c68748f2c5e5eca5837bfb8bb152b31
Armin-Ariamajd <80158628+Armin-Ariamajd@users.noreply.github.com>
2022-01-23 02:38:35 +0100

    renamed 'element_masses.py' to 'atom_data.py' and the dict inside from 'masses' to 'mass_dalton'

---
+++ mdforce/data/atom_data.py
@@ -0,0 +1,8 @@
+"""
+Atomic data for all elements.
+"""
+
+mass_dalton = {
+    1: 1.008,
+    8: 15.999,
+}
--- mdforce/data/element_masses.py
+++
@@ -1,8 +0,0 @@
-"""
-Mass of elements in Dalton.
-"""
-
-masses = {
-    1: 1.008,
-    8: 15.999,
-}
