a759a2bb4f781b2b93d95e69bd49785b1a49944b
Armin-Ariamajd <80158628+Armin-Ariamajd@users.noreply.github.com>
2022-01-25 16:26:53 +0100

    updated all functions and docstrings

--- mdforce/terms_multi_iter.py
+++ mdforce/terms_multi_iter.py
@@ -1,185 +1,193 @@
 """
-Implementation of the individual terms of a general force field. Each function calculates the force
-for a number of particle-pairs – or in case of the angle vibration, particle-triplets – in an iterative way.
-Moreover, each function also returns the potential energy of each particle-pair/triplet.
+Implementation of the individual terms of a general force field.
+
+Each function calculates the force for a given number of particle-pairs (or particle-triplets in
+case of the angle-vibration function) in an iterative way. Moreover, each function also returns the
+potential energy of each particle-pair/triplet.
 """
 
-
+# Standard library
+from typing import Tuple
+# 3rd-party packages
 import numpy as np
-
+# Self
 from . import terms_single_array
 
 
-def lennard_jones(q, pair_idx, ab):
+def coulomb(
+        q: np.ndarray, pairs_idx: np.ndarray, c: np.ndarray, k_e: float
+) -> Tuple[np.ndarray, np.ndarray]:
     """
-    Calculate the total Lennard-Jones potential and force
-    for a number of particles, due to other particles.
+    Calculate the coulomb potential between a number of particle-pairs, and force on each particle.
 
     Parameters
     ----------
     q : numpy.ndarray
-        2D array of shape (n, m), containing the coordinates
-        of n particles in an m-dimensional space.
-    pair_idx : numpy.ndarray
-        2D array of shape (p, 2), containing the indices
-        of all pairs of particles in q, between which
-        the Lennard-Jones interaction should be calculated.
-    ab : numpy.ndarray
-        2D array of shape (p, 2), containing the parameters
-        A and B , respectively, for each pair in pair_idx.
-
-    Returns
-    -------
-        tuple [numpy.ndarray, numpy.ndarray]
-        Force vectors for all particles, as a 2D array of shape (n, m)
-        followed by potential energy of all particles, as a 1D array of length (n).
-
-    Notes
-    -----
-    For particles in q whose index is not in pair_idx,
-    the respective positions in returned force and potential arrays
-    will be zero.
-    """
-
-    f = np.zeros_like(q)
-    e = np.zeros(q.shape[0])
-    for idx, (i, j) in enumerate(pair_idx):
-        f_ij, e_ij = terms_single_array.lennard_jones(q[i], q[j], ab[idx, 0], ab[idx, 1])
-        f[[i, j]] += f_ij, -f_ij
-        e[[i, j]] += e_ij
-    return f, e
-
-
-def coulomb(q, pair_idx, c, k):
-    """
-    Calculate the total Coulomb potential and force
-    for a number of particles, due to other particles.
-
-    Parameters
-    ----------
-    q : numpy.ndarray
-        2D array of shape (n, m), containing the coordinates
-        of n particles in an m-dimensional space.
-    pair_idx : numpy.ndarray
-        2D array of shape (p, 2), containing the indices
-        of all pairs of particles in q, between which
-        the Coulomb interaction should be calculated.
+        Coordinates vectors of particles as a 2D-array of shape (n, m), where 'n' is the number of
+        particles, and 'm' is the number of spatial dimensions for particles' coordinates.
+    pairs_idx : numpy.ndarray
+        Indices of all pairs of particles in `q`, between which the interaction should be
+        calculated, as a 2D-array of shape (p, 2), where 'p' is the number of interacting pairs.
     c : numpy.ndarray
-        1D array of length (n), containing the
-        charges of all particles in q.
-    k : float
+        Charges of all particles in `q`, as a 1D-array of shape (n, ). The value at each index
+        corresponds to the charge of the particle at the same index in `q`.
+    k_e : float
         Coulomb constant, i.e. (1 / 4πε0).
 
     Returns
     -------
-        tuple [numpy.ndarray, numpy.ndarray]
-        Force vectors for all particles, as a 2D array of shape (n, m)
-        followed by potential energy of all particles, as a 1D array of length (n).
+    force, potential : Tuple[numpy.ndarray, numpy.ndarray]
+        Force vectors for all particles, as a 2D-array of shape (n, m), followed by the potential
+        energy between each pair, as a 1D-array of shape (p, ).
 
     Notes
     -----
-    For particles in q whose index is not in pair_idx,
-    the respective positions in returned force and potential arrays
-    will be zero.
+    For particles in `q` whose index is not in `pairs_idx`, the respective positions in returned
+    force and potential arrays will be zero.
     """
-
-    f = np.zeros_like(q)
-    e = np.zeros(q.shape[0])
-    for i, j in pair_idx:
-        f_ij, e_ij = terms_single_array.coulomb(q[i], q[j], c[i], c[j], k)
+    # Initialize arrays to store force and potential
+    f = np.zeros(q.shape)
+    pot = np.zeros(pairs_idx.shape)
+    # Iterate over all particle-pair indices, and calculate force and potential
+    for pair_idx, (i, j) in enumerate(pairs_idx):
+        f_ij, pot_ij = terms_single_array.coulomb(q[i], q[j], c[i], c[j], k_e)
         f[[i, j]] += f_ij, -f_ij
-        e[[i, j]] += e_ij
-    return f, e
+        pot[pair_idx] = pot_ij
+    return f, pot
 
 
-def bond_vibration_harmonic(q, pair_idx, eq_dist, k):
+def lennard_jones(
+        q: np.ndarray, pairs_idx: np.ndarray, a: np.ndarray, b: np.ndarray
+) -> Tuple[np.ndarray, np.ndarray]:
     """
-    Calculate the total bond-vibration potential and force
-    for a number of bonded pairs of particles, using the
-    harmonic oscillator model.
+    Calculate the Lennard-Jones potential between a number of particle-pairs, and force on each
+    particle.
 
     Parameters
     ----------
     q : numpy.ndarray
-        2D array of shape (n, m), containing the coordinates
-        of n particles in an m-dimensional space.
-    pair_idx : numpy.ndarray
-        2D array of shape (p, 2), containing the indices
-        of all pairs of particles in q, between which
-        the bond-vibration interaction should be calculated.
+        Coordinates vectors of particles as a 2D-array of shape (n, m), where 'n' is the number of
+        particles, and 'm' is the number of spatial dimensions for particles' coordinates.
+    pairs_idx : numpy.ndarray
+        Indices of all pairs of particles in `q`, between which the interaction should be
+        calculated, as a 2D-array of shape (p, 2), where 'p' is the number of interacting pairs.
+    a : numpy.ndarray
+        A-parameter of the Lennard-Jones potential for each particle-pair in `pairs_idx`, as a
+        1D-array of shape (p, ).
+    b : numpy.ndarray
+        B-parameter of the Lennard-Jones potential for each particle-pair in `pairs_idx`, as a
+        1D-array of shape (p, ).
+
+    Returns
+    -------
+    force, potential : Tuple[numpy.ndarray, numpy.ndarray]
+        Force vectors for all particles, as a 2D-array of shape (n, m), followed by the potential
+        energy between each pair, as a 1D-array of shape (p, ).
+
+    Notes
+    -----
+    For particles in `q` whose index is not in `pairs_idx`, the respective positions in returned
+    force and potential arrays will be zero.
+    """
+    # Initialize arrays to store force and potential
+    f = np.zeros(q.shape)
+    pot = np.zeros(pairs_idx.shape)
+    # Iterate over all particle-pair indices, and calculate force and potential
+    for pair_idx, (i, j) in enumerate(pairs_idx):
+        f_ij, pot_ij = terms_single_array.lennard_jones(q[i], q[j], a[pair_idx], b[pair_idx])
+        f[[i, j]] += f_ij, -f_ij
+        pot[pair_idx] = pot_ij
+    return f, pot
+
+
+def bond_vibration_harmonic(
+        q: np.ndarray, pairs_idx: np.ndarray, eq_dist: np.ndarray, k_b: np.ndarray
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Calculate the harmonic bond vibration potential between a number of particle-pairs, and force
+    on each particle.
+
+    Parameters
+    ----------
+    q : numpy.ndarray
+        Coordinates vectors of particles as a 2D-array of shape (n, m), where 'n' is the number of
+        particles, and 'm' is the number of spatial dimensions for particles' coordinates.
+    pairs_idx : numpy.ndarray
+        Indices of all pairs of particles in `q`, between which the interaction should be
+        calculated, as a 2D-array of shape (p, 2), where 'p' is the number of interacting pairs.
     eq_dist : numpy.ndarray
-        1D array of length (p), containing the
-        equilibrium bond length for each pair in pair_idx.
-    k : float
-        force constant of the harmonic potential.
+        Equilibrium bond length for each particle-pair in `pairs_idx`, as a 1D-array of shape
+        (p, ).
+    k_b : float
+        Force constant of the harmonic bond potential for each particle-pair in `pairs_idx`, as a
+        1D-array of shape (p, ).
 
     Returns
     -------
-        tuple [numpy.ndarray, numpy.ndarray]
-        Force vectors for all particles, as a 2D array of shape (n, m)
-        followed by potential energy of all particles, as a 1D array of length (n).
+    force, potential : Tuple[numpy.ndarray, numpy.ndarray]
+        Force vectors for all particles, as a 2D-array of shape (n, m), followed by the potential
+        energy between each pair, as a 1D-array of shape (p, ).
 
     Notes
     -----
-    For particles in q whose index is not in pair_idx,
-    the respective positions in returned force and potential arrays
-    will be zero.
+    For particles in `q` whose index is not in `pairs_idx`, the respective positions in returned
+    force and potential arrays will be zero.
     """
-
-    f = np.zeros_like(q)
-    e = np.zeros(q.shape[0])
-    for idx, (i, j) in enumerate(pair_idx):
-        f_ij, e_ij = terms_single_array.bond_vibration_harmonic(q[i], q[j], eq_dist[idx], k)
+    # Initialize arrays to store force and potential
+    f = np.zeros(q.shape)
+    pot = np.zeros(pairs_idx.shape[0])
+    # Iterate over all particle-pair indices, and calculate force and potential
+    for pair_idx, (i, j) in enumerate(pairs_idx):
+        f_ij, pot_ij = terms_single_array.bond_vibration_harmonic(
+            q[i], q[j], eq_dist[pair_idx], k_b[pair_idx]
+        )
         f[[i, j]] += f_ij, -f_ij
-        e[[i, j]] += e_ij
-    return f, e
+        pot[pair_idx] = pot_ij
+    return f, pot
 
 
-def angle_vibration_harmonic(q, pair_idx, eq_angle, k):
+def angle_vibration_harmonic(
+        q: np.ndarray, triplets_idx: np.ndarray, eq_angle: np.ndarray, k_a: np.ndarray
+) -> Tuple[np.ndarray, np.ndarray]:
     """
-    Calculate the total angle-vibration potential and force
-    for a number of bonded triplets of particles, using the
-    harmonic oscillator model.
+    Calculate the harmonic angle vibration potential between a number of particle-triplets, and
+    force on each particle.
 
     Parameters
     ----------
     q : numpy.ndarray
-        2D array of shape (n, m), containing the coordinates
-        of n particles in an m-dimensional space.
-    pair_idx : numpy.ndarray
-        2D array of shape (p, 3), containing the indices
-        of all triplets of particles in q, between which
-        the angle-vibration interaction should be calculated.
-        For each triplet, the index of the particle in the middle
-        should be the first index
+        Coordinates vectors of particles as a 2D-array of shape (n, m), where 'n' is the number of
+        particles, and 'm' is the number of spatial dimensions for particles' coordinates.
+    triplets_idx : numpy.ndarray
+        Indices of all triplets of particles in `q`, between which the interaction should be
+        calculated, as a 2D-array of shape (p, 3), where 'p' is the number of interacting pairs.
+        For each triplet, the index of the particle in the middle should be the first index.
     eq_angle : numpy.ndarray
-        1D array of length (p), containing the
-        equilibrium angle for each triplet in pair_idx.
-    k : float
-        force constant of the harmonic potential.
+        Equilibrium angle (in radian) for each particle-triplet in `triplets_idx`, as a 1D-array of
+        shape (p, ).
+    k_a : numpy.ndarray
+        Force constant of the harmonic angle potential for each particle-triplet in `triplets_idx`,
+        as a 1D-array of shape (p, ).
 
     Returns
     -------
-        tuple [numpy.ndarray, numpy.ndarray]
-        Force vectors for all particles, as a 2D array of shape (n, m)
-        followed by potential energy of all particles, as a 1D array of length (n).
+    force, potential : Tuple[numpy.ndarray, numpy.ndarray]
+        Force vectors for all particles, as a 2D-array of shape (n, m), followed by the potential
+        energy between each triplet, as a 1D-array of shape (p, ).
 
     Notes
     -----
-    For particles in q whose index is not in pair_idx,
-    the respective positions in returned force and potential arrays
-    will be zero.
+    For particles in `q` whose index is not in `pairs_idx`, the respective positions in returned
+    force and potential arrays will be zero.
     """
-
-    f = np.zeros_like(q)
-    e = np.zeros(q.shape[0])
-    # FIXME
-    # remove angle (added for debugging)
-    for idx, (m, l, r) in enumerate(pair_idx):
-        f_m, f_l, f_r, e_mlr, angle = terms_single_array.angle_vibration_harmonic(
-            q[m], q[l], q[r], eq_angle[idx], k
+    # Initialize arrays to store force and potential
+    f = np.zeros(q.shape)
+    pot = np.zeros(triplets_idx.shape[0])
+    # Iterate over all particle-pair indices, and calculate force and potential
+    for triplet_idx, (j, i, k) in enumerate(triplets_idx):
+        f_j, f_i, f_k, pot_ijk = terms_single_array.angle_vibration_harmonic(
+            q[j], q[i], q[k], eq_angle[triplet_idx], k_a[triplet_idx]
         )
-        f[[m, l, r]] += f_m, f_l, f_r
-        e[[m, l, r]] += e_mlr
-
-    return f, e, angle
+        f[[j, i, k]] += f_j, f_i, f_k
+        pot[triplet_idx] = pot_ijk
+    return f, pot
